bfa806bc71292c4f4531f5296dfd93eee8b932a0
Santiago Pattacini <santiagopattacini@gmail.com>
2022-10-27 23:37:58 -0300

    estilos iniciales

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <img src="./img/logo.png" className="header__logo" alt="logo" />
+        <div className='header__nav'>
+          <h3>Home</h3>
+          <h3>Shorts</h3>
+          <h3>Gorras</h3>
+        </div>
+        <div className='header_buttons'>Comprar</div>
       </header>
     </div>
   );
